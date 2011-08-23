906b1388382f19c19d838c6ae74718deb3485bd1
kende <kende.j@gmail.com>
2011-08-23 15:01:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenarrows::Application.config.secret_token = 'f965ff38e511c4862a5267f2248542af57f18dfee0735e7f772703998a14ba95cd116a9b26169ce1ce4f708fe68664d63c01fcef4247818a545af7f0b15651fa'
